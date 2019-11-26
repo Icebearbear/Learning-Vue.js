29f551e767a4c117f5be537589a62dd1925fd7e0
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-11-27 01:04:29 +0800

    Fixed formatting of repo with eslint: 1.  Updated .eslint.js config file 2.  Updated package.json, removed inner eslint config. 3.  All other .JS and .vue files, modified as part of eslint format --fix

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,36 @@
-<template>  
+<template>
   <div id="app">
-    <Apphome v-bind:foods="foods"/>
-  
+    <Apphome v-bind:foods="foods" />
 
-  <!-- retrieve data from array -->
-  <h1 class="text">What have you eat today?</h1> 
-
-
+    <!-- retrieve data from array -->
+    <h1 class="text">What have you eat today?</h1>
   </div>
 </template>
 
 
-
 <script>
-
-import Apphome from './components/AppHome.vue';
-
+import Apphome from "./components/AppHome.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Apphome
-
   },
   data() {
     return {
-      foods : [
-        {id: 1, name : 'cheese', au: 'lel'},
-        {id : 2, name: 'choco' , au:'lol'},
-        {id: 3, name: 'cream', au:'lal'}
-
-        
-
-      ]   //array of objects
-    }
-  },
-
+      foods: [
+        { id: 1, name: "cheese", au: "lel" },
+        { id: 2, name: "choco", au: "lol" },
+        { id: 3, name: "cream", au: "lal" }
+      ] //array of objects
+    };
   }
-
-
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue' // component
+import Vue from 'vue';
+import App from './App.vue'; // component
 // import AppHome from './components/AppHome.vue'
 // import Router from 'vue-router'
 // Vue.use(Router);
 
 // Vue.component('app-home', AppHome);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 //ViewModel
 new Vue({   //rendering new Vue instance 
   render: h => h(App),    //rendering App component 
-}).$mount('#app')       //inside an element with id of app
+}).$mount('#app');       //inside an element with id of app
