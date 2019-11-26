d13f54a32045059507d5d7e486afad71b07ca41e
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-11-27 01:05:45 +0800

    Fixed prop updating bug in "AppHome" component:     Modified to pass in the new input value to the method rather than     using v-model binding.

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -1,56 +1,42 @@
 
 
 <template>
-    <div>
+  <div>
     <!-- <h2 class="text">Sally is here to interrogate you!</h2>
-    <img id="img" src="../assets/sally.png"> -->
-  
+    <img id="img" src="../assets/sally.png">-->
+
     <ul>
-    <li v-bind:key="food.index" v-for="food of foods">{{food.name}}</li>
-    </ul> 
+      <li v-for="food of foods" v-bind:key="food.id">{{food.name}}</li>
+    </ul>
 
-    <input v-model="message" placeholder="type here" :key="index" value="">
-    <button type="button" class="submit_button" @click="addFood">Add</button>
- 
-
-  
-</div>
+    <input v-model="input" placeholder="type here" />
+    <button type="button" class="submit_button" @click="addFood(input)">Add</button>
+  </div>
 </template>
 
 <script>
-
 export default {
-    name: "Apphome",
-    components: {
-    
-
-    },
-    props: ["foods"],
-    methods: {
-        addFood : function(){
-            // console.log(this.foods)
-            this.foods.push({name: value})
-        }
+  name: "Apphome",
+  components: {},
+  props: ["foods"],
+  methods: {
+    addFood: function(food) {
+      this.$props.foods.push({ name: food });
     }
-  
-}
+  }
+};
 </script>
 
 <style scoped>
-
-
 input {
-    width: 250px;
-    height: 50px;
-    top: 80%;
+  width: 250px;
+  height: 50px;
+  top: 80%;
 }
 
 button {
-    width: 150px;
-    height: 50px;
-    top: 80%
+  width: 150px;
+  height: 50px;
+  top: 80%;
 }
-
 </style>
-
-
